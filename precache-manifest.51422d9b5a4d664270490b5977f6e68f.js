302c34a4b9c5f99417e0f125a9ebc6c08caf24f8
crystallynnv <crystal.lynnv@gmail.com>
2020-04-14 10:51:20 -0500

    Updates

---
+++ precache-manifest.51422d9b5a4d664270490b5977f6e68f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b52b6cf4fbf86be1b02f229b7a4741b1",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "3b1b030a512396819d68",
+    "url": "/project-4-assessment/static/css/main.56b20a1e.chunk.css"
+  },
+  {
+    "revision": "bf9faed5984ec60f7781",
+    "url": "/project-4-assessment/static/js/2.36c0ca1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.36c0ca1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b1b030a512396819d68",
+    "url": "/project-4-assessment/static/js/main.49ff4de5.chunk.js"
+  },
+  {
+    "revision": "bb8b828653ad1446ab40",
+    "url": "/project-4-assessment/static/js/runtime-main.58f97c80.js"
+  }
+]);
